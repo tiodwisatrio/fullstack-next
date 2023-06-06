303d05ad060e5a284d0141e93d5a6e45717da72e
tiodwisatrio <tiodwisatrio27@gmail.com>
2023-06-06 23:48:49 +0700

    making a project and detail project pages

--- src/app/project/[category]/page.jsx
+++ src/app/project/[category]/page.jsx
@@ -10,14 +10,80 @@ const ProjectCategory = ({ params }) => {
       <h1 className={styles.catTitle}>{params.category}</h1>
       <div className={styles.item}>
         <div className={styles.content}>
-          <h1 className={styles.title}>Test</h1>
-          <p className={styles.desc}>Desc</p>
+          <h1 className={styles.title}>My Illustrations</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            itaque fuga culpa perferendis, consectetur dignissimos aliquid
+            consequatur minima praesentium, minus dolor consequuntur sed
+            cupiditate temporibus.
+          </p>
           <Button text="See More" url="#" />
         </div>
+
         <div className={styles.imgContainer}>
           <Image
             fill={true}
-            src="https://images.pexels.com/photos/159862/art-school-of-athens-raphael-italian-painter-fresco-159862.jpeg?auto=compress&cs=tinysrgb&w=300"
+            src="https://images.pexels.com/photos/2860804/pexels-photo-2860804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>My Illustrations</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            itaque fuga culpa perferendis, consectetur dignissimos aliquid
+            consequatur minima praesentium, minus dolor consequuntur sed
+            cupiditate temporibus.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/2860804/pexels-photo-2860804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>My Illustrations</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            itaque fuga culpa perferendis, consectetur dignissimos aliquid
+            consequatur minima praesentium, minus dolor consequuntur sed
+            cupiditate temporibus.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/2860804/pexels-photo-2860804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            className={styles.image}
+          />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>My Illustrations</h1>
+          <p className={styles.desc}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+            itaque fuga culpa perferendis, consectetur dignissimos aliquid
+            consequatur minima praesentium, minus dolor consequuntur sed
+            cupiditate temporibus.
+          </p>
+          <Button text="See More" url="#" />
+        </div>
+
+        <div className={styles.imgContainer}>
+          <Image
+            fill={true}
+            src="https://images.pexels.com/photos/2860804/pexels-photo-2860804.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
             className={styles.image}
           />
         </div>
